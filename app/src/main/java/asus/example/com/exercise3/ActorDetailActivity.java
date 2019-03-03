567da94b829c47f194f9da50a159e1bb3864e18e
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-03-03 17:08:44 +0300

    Added integers and changed Manifest

--- app/src/main/java/asus/example/com/exercise3/ActorDetailActivity.java
+++ app/src/main/java/asus/example/com/exercise3/ActorDetailActivity.java
@@ -1,7 +1,5 @@
 package asus.example.com.exercise3;
 
-import android.content.res.Configuration;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -11,40 +9,17 @@ import com.bumptech.glide.Glide;
 
 public class ActorDetailActivity extends AppCompatActivity {
 
-    private String uri;
-    private String sInformation;
-    private ImageView avatar;
-    private TextView information;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actor_detail);
         int position = getIntent().getIntExtra("position", 0);
-        sInformation = DataUtil.generateActors().get(position).getDescription();
-        uri = DataUtil.generateActors().get(position).getLargeImage().toString();
-        initializeView();
+        ImageView avatar = findViewById(R.id.big_avatar);
+        TextView information = findViewById(R.id.information);
+        Glide.with(getApplicationContext()).load(DataUtil.generateActors().get(position).getLargeImage()).into(avatar);
+        information.setText(DataUtil.generateActors().get(position).getDescription());
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig){
-        super.onConfigurationChanged(newConfig);
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
-            setContentView(R.layout.activity_actor_detail);
-            initializeView();
-        }
-        else {
-            setContentView(R.layout.activity_actor_detail);
-            initializeView();
-        }
-    }
-
-    private void initializeView(){
-        avatar = findViewById(R.id.big_avatar);
-        information = findViewById(R.id.information);
-        Glide.with(getApplicationContext()).load(Uri.parse(uri)).into(avatar);
-        information.setText(sInformation);
-    }
 
 }
--- app/src/main/java/asus/example/com/exercise3/GalleryActivity.java
+++ app/src/main/java/asus/example/com/exercise3/GalleryActivity.java
@@ -2,7 +2,6 @@ package asus.example.com.exercise3;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -13,23 +12,16 @@ import java.util.List;
 
 public class GalleryActivity extends AppCompatActivity {
 
-    private RecyclerView previewImages;
     private final List<Actor> actors = DataUtil.generateActors();
-
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
-        previewImages = findViewById(R.id.previewImages);
+        RecyclerView previewImages = findViewById(R.id.previewImages);
         previewImages.addItemDecoration(new SpacesItemDecoration(4));
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            previewImages.setLayoutManager(new GridLayoutManager(this, 2));
-        }
-        else if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            previewImages.setLayoutManager(new GridLayoutManager(this, 4));
-        }
+        previewImages.setLayoutManager(new GridLayoutManager(this, getResources().getInteger(R.integer.rows)));
 
 
 
@@ -46,29 +38,5 @@ public class GalleryActivity extends AppCompatActivity {
 
 
     }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig){
-        super.onConfigurationChanged(newConfig);
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
-            previewImages.setLayoutManager(new GridLayoutManager(this, 4));
-        }
-        else {
-            previewImages.setLayoutManager(new GridLayoutManager(this, 2));
-        }
-    }
-
-//    @Override
-//    public void onSaveInstanceState(Bundle outState) {
-//        super.onSaveInstanceState(outState);
-//        outState.putBundle("BUNDLE", bundle);
-//    }
-//
-//    @Override
-//    protected void onRestoreInstanceState(Bundle onSavedInstance){
-//        super.onRestoreInstanceState(onSavedInstance);
-//        onSavedInstance.getBundle("BUNDLE");
-//    }
-
 
 }
